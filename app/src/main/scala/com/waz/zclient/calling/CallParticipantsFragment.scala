d78491ee392794849e60f90b19ee21a466174cc7
dean <deanrobertcook@gmail.com>
2018-05-15 17:56:29 +0200

    Remove fade from full participants list

--- app/src/main/scala/com/waz/zclient/calling/CallParticipantsFragment.scala
+++ app/src/main/scala/com/waz/zclient/calling/CallParticipantsFragment.scala
@@ -21,43 +21,23 @@ import android.os.Bundle
 import android.support.v7.widget.Toolbar
 import android.view.{LayoutInflater, View, ViewGroup}
 import com.waz.ZLog.ImplicitTag._
-import com.waz.utils.events.Subscription
-import com.waz.zclient.R
 import com.waz.zclient.calling.controllers.CallController
 import com.waz.zclient.calling.views.CallParticipantsView
-import com.waz.zclient.utils.RichView
+import com.waz.zclient.{FragmentHelper, R}
 
-class CallParticipantsFragment extends FadingControls {
+class CallParticipantsFragment extends FragmentHelper {
   private lazy val toolbar = view[Toolbar](R.id.toolbar)
   private lazy val participantsView = view[CallParticipantsView](R.id.full_call_participants)
 
   private lazy val controller = inject[CallController]
-
-  private var subs = Set.empty[Subscription]
 
   override def onCreateView(inflater: LayoutInflater, container: ViewGroup, savedInstanceState: Bundle) =
     inflater.inflate(R.layout.fragment_calling_participants, container, false)
 
   override def onViewCreated(view: View, savedInstanceState: Bundle): Unit = {
     super.onViewCreated(view, savedInstanceState)
-
-    view.onClick(if (controller.showVideoView.currentValue.getOrElse(false)) toggleControlVisibility())
-
     toolbar
     participantsView
-
-    setFadingControls(toolbar.get, participantsView.get)
-
-    subs += controller.showVideoView.onUi {
-      case true  => extendControlsDisplay()
-      case false => stopFadeOut()
-    }
-  }
-
-  override def onDestroyView(): Unit = {
-    subs.foreach(_.destroy())
-    subs = Set.empty
-    super.onDestroyView()
   }
 
   override def onResume(): Unit = {
